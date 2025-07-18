5057c8921ed9178feb16bea1ba21a86c474a286d
QuangBinh <ngqbinh456@gmail.com>
2025-07-18 14:20:38 +0700

    Upload additional files

--- project-proposal/Project/backend/application.py
+++
@@ -1,5 +0,0 @@
-# Elastic Beanstalk entry point
-from app import app
-
-if __name__ == "__main__":
-    app.run(debug=False)
--- project-proposal/Project/backend/lambda_handler.py
+++
@@ -1,9 +0,0 @@
-import serverless_wsgi
-from app import app
-
-def lambda_handler(event, context):
-    return serverless_wsgi.handle_request(app, event, context)
-
-# For local testing
-if __name__ == "__main__":
-    app.run(debug=True)
